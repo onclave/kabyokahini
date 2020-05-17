a49d463e60be8461692add3cb134677cc6924be5
Debabrata Acharya <sajib@Debabratas-MacBook-Pro.local>
2020-05-17 18:29:52 +0530

    integrate with CI/CD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatMenuModule } from '@angular/material/menu';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
